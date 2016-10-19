107575b00e6d87b3b221675f67c6389584baa4d3
lsp <729789688@qq.com>
2016-10-19 22:57:39 +0800

    测试代码更新

--- app/src/main/java/com/lsp/views/MainActivity.java
+++ app/src/main/java/com/lsp/views/MainActivity.java
@@ -2,43 +2,29 @@ package com.lsp.views;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.view.Gravity;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.TextView;
 
-import com.lsp.swipeMenuList.*;
 
 public class MainActivity extends AppCompatActivity {
-    private SwipeMenuListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        listView = (SwipeMenuListView)findViewById(R.id.sml);
+
+        TextView tv = new TextView(this);
+
+        tv.setWidth(ViewGroup.LayoutParams.MATCH_PARENT);
+        tv.setHeight(ViewGroup.LayoutParams.MATCH_PARENT);
+
+        tv.setText("草泥马");
+        tv.setGravity(Gravity.RIGHT);
+
+        setContentView(tv);
+
 
     }
 
-}
- class swipeMenuListAdapter extends BaseAdapter {
-
-    @Override
-    public int getCount() {
-        return 0;
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return 0;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
-    }
 }
