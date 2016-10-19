0eb4cc015e80287e17ae92eadb4714a5db352ea5
lsp <729789688@qq.com>
2016-10-19 22:20:16 +0800

    测试代码更新

---
+++ app/src/main/java/com/lsp/Test/Test.java
@@ -0,0 +1,11 @@
+package com.lsp.Test;
+
+/**
+ * Created by Administrator on 2016/10/19.
+ */
+public class Test {
+    public static Test getTest(){
+        return new Test();
+    }
+    private Test(){}
+}
